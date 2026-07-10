b471b4a1cbaf7bda0d0775e23dd3bb6e35d469f6
Misyutin.IA <sepot1009@gmail.com>
2026-07-10 14:33:44 +0300

    init

---
+++ src/app/main/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <main>
+                <p className="text-red-500">MAIN PAGE</p>
+            </main>
+        </div>
+    );
+}
